fe065163614c4658b99cf1eeeb58b814a27124da
Tyler Dong <tylerdong878@gmail.com>
2025-10-05 03:37:43 -0400

    update vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,11 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import basicSsl from '@vitejs/plugin-basic-ssl';
 import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss(), basicSsl()],
+	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
@@ -15,6 +14,5 @@ export default defineConfig({
 	server: {
 		host: '0.0.0.0',
 		port: 5173,
-		https: true,
 	},
 });
